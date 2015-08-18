bca5e86f640905a0a7b238c4e31cfb2ee8f4ae7d
wl_kayak <wl89792978>
2015-08-18 12:03:51 +0800

    wd test 12:03

--- Hellow-Mevan Maven Webapp/src/main/java/org/wd/hello/HelloAction.java
+++ Hellow-Mevan Maven Webapp/src/main/java/org/wd/hello/HelloAction.java
@@ -2,5 +2,7 @@ package org.wd.hello;
 public class HelloAction {
 	public static void main(String[] args) {
 		System.out.println("wd test git...");
+		System.out.println("wd test git 2.........");
+
 	}
 }
