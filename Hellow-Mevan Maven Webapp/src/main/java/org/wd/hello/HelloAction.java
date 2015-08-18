dee9383e40ef809b314554e03aa7d774ab56d06e
wl_kayak <wl89792978>
2015-08-18 11:52:27 +0800

    wd test

--- Hellow-Mevan Maven Webapp/src/main/java/org/wd/hello/HelloAction.java
+++ Hellow-Mevan Maven Webapp/src/main/java/org/wd/hello/HelloAction.java
@@ -1,19 +1,6 @@
 package org.wd.hello;
-
-import org.apache.commons.httpclient.HttpClient;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-@Controller
 public class HelloAction {
-	@RequestMapping("hello/{wd}")
-	@ResponseBody
-	public String hello(@PathVariable("wd") String wd) {
-
-	
-		
-		return "Hello " + wd;
+	public static void main(String[] args) {
+		System.out.println("wd test git...");
 	}
 }
